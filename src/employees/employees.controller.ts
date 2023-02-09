6d4b108c0100cb32aabf3885a3d8ca9f9f769838
Bruno Henrique <bruno.henrique@imobpower.com>
2023-02-09 17:16:32 -0300

    final

---
+++ src/employees/employees.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Inject,
+} from '@nestjs/common';
+import { EmployeesService } from './employees.service';
+import { CreateEmployeeDto } from './dto/create-employee.dto';
+import { UpdateEmployeeDto } from './dto/update-employee.dto';
+
+@Controller('employees')
+export class EmployeesController {
+  constructor(
+    @Inject(EmployeesService)
+    private readonly employeesService: EmployeesService,
+  ) {}
+
+  @Post()
+  create(@Body() createEmployeeDto: CreateEmployeeDto) {
+    return this.employeesService.create(createEmployeeDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.employeesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.employeesService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateEmployeeDto: UpdateEmployeeDto,
+  ) {
+    return this.employeesService.update(id, updateEmployeeDto);
+  }
+
+  @Patch('vacation/:id')
+  updateVacation(@Param('id') id: string, @Body() data: { status: boolean }) {
+    return this.employeesService.updateVacation(id, data.status);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.employeesService.remove(id);
+  }
+}
