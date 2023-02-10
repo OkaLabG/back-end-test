432c36fa1a438fb4469a02cdda9d13fd309d3023
Bruno Henrique <brunohqsantos@hotmail.com>
2023-02-09 21:26:41 -0300

    crud users

---
+++ src/app/users/entities/user.entity.ts
@@ -0,0 +1,32 @@
+import {
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ObjectID,
+  ObjectIdColumn,
+} from 'typeorm';
+import { hashSync } from 'bcrypt';
+
+@Entity()
+export class User {
+  @ObjectIdColumn()
+  _id: ObjectID;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @BeforeInsert()
+  hashPassword() {
+    this.password = hashSync(this.password, 10);
+  }
+}
